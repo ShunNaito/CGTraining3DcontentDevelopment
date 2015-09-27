bd2f4a8542534ce8c640960e5a88c2a4947cf8cb
Shun <s.n.systems.engineer@gmail.com>
2015-09-27 17:01:16 +0900

    最初のコミットです

---
+++ 01/Assets/BallContoller.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var Speed = 20.0;
+function Start () {
+	var rb = GetComponent.<Rigidbody>();
+	rb.AddForce((transform.forward + transform.right) * Speed, ForceMode.VelocityChange);
+}
+
+function Update () {
+
+}
