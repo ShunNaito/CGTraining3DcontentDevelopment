d72856707b7896ec0b6ea7c08052d61bb644b47c
Shun <s.n.systems.engineer@gmail.com>
2015-09-27 17:44:53 +0900

    ラケットを操作可能にした

---
+++ 01/Assets/RacketController.js
@@ -0,0 +1,8 @@
+﻿#pragma strict
+
+var Accel = 1000.0;
+
+function Update () {
+	var rb = GetComponent.<Rigidbody>();
+	rb.AddForce(transform.right * Input.GetAxisRaw("Horizontal") * Accel, ForceMode.Impulse);
+}
