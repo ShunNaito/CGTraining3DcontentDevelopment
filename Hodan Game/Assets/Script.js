0885a19518f422648579d305d05ab7f8d930020a
Shun <s.n.systems.engineer@gmail.com>
2015-10-26 16:32:23 +0900

    砲弾ゲームを追加

---
+++ Hodan Game/Assets/Script.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var bullet : Rigidbody;
+var power : int = 50;
+private var distance : int = 100;
+
+function Start() {
+}
+
+function Update() {
+  if(Input.GetButtonDown("Fire1")) {
+    var clone : Rigidbody = Instantiate(bullet, transform.position, transform.rotation);
+		
+    var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		
+    var point : Vector3 = ray.origin + (ray.direction * distance);
+		
+    var dir : Vector3 = point - transform.position;
+		
+    dir.Normalize();
+		
+    clone.velocity = transform.TransformDirection(dir * power);
+		
+    Destroy(clone.gameObject, 3);
+  }
+}
